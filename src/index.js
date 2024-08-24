6a96426c28d5f1eefe4622b13e7288ab9be98a3f
Diana Butiakova <butyakova01@mail.ru>
2024-08-24 17:57:12 +0600

    feat: check brackets str works

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = 
+function check(str, bracketsConfig) {
+  const brackets = bracketsConfig.reduce((a, [o, c]) => {a[c] = o; return a}, {});
+  const openBrackets = bracketsConfig.map(([o, c]) => o);
+  const lastOpenBrackets = [];
+  for (let i = 0; i < str.length; i++) {
+    if (lastOpenBrackets.length !== 0 && lastOpenBrackets.slice(-1)[0] === brackets[str[i]]) {
+      lastOpenBrackets.pop();
+    } else if (openBrackets.includes(str[i])) {
+      lastOpenBrackets.push(str[i]);
+    } else {
+      return false;
+    }
+  }
+  return lastOpenBrackets.length === 0;
 }
